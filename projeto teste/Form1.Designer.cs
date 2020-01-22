de90a3aae79760a13d606fbd603da487cb1e638b
felip <felip@DESKTOP-4LS6LFN>
2020-01-22 17:12:12 -0300

    Primeiro Commit

---
+++ projeto teste/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace projeto_teste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtCaixa1 = new System.Windows.Forms.TextBox();
+            this.txtCaixa2 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.cmbOperador = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCaixa1
+            // 
+            this.txtCaixa1.Location = new System.Drawing.Point(108, 79);
+            this.txtCaixa1.Name = "txtCaixa1";
+            this.txtCaixa1.Size = new System.Drawing.Size(119, 20);
+            this.txtCaixa1.TabIndex = 0;
+            // 
+            // txtCaixa2
+            // 
+            this.txtCaixa2.Location = new System.Drawing.Point(106, 133);
+            this.txtCaixa2.Name = "txtCaixa2";
+            this.txtCaixa2.Size = new System.Drawing.Size(121, 20);
+            this.txtCaixa2.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(138, 185);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(89, 42);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnSoma_Click);
+            // 
+            // cmbOperador
+            // 
+            this.cmbOperador.FormattingEnabled = true;
+            this.cmbOperador.Items.AddRange(new object[] {
+            "Somar",
+            "Subtrair",
+            "Multiplicar",
+            "Dividir"});
+            this.cmbOperador.Location = new System.Drawing.Point(106, 31);
+            this.cmbOperador.Name = "cmbOperador";
+            this.cmbOperador.Size = new System.Drawing.Size(121, 21);
+            this.cmbOperador.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Numero1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Numero2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Operador:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 332);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbOperador);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtCaixa2);
+            this.Controls.Add(this.txtCaixa1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Primeiro Projeto Felipe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCaixa1;
+        private System.Windows.Forms.TextBox txtCaixa2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ComboBox cmbOperador;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ projeto teste/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projeto_teste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+
+            // verifica se o operador foi escolhido
+            if(cmbOperador.Text != "")
+            {
+                int numero1 = Convert.ToInt32(txtCaixa1.Text); ; // convert o text do txtCaixa1 para int
+                int numero2 = Convert.ToInt32(txtCaixa2.Text); ; // ||      ||   ||  do txtcaixa2 para int
+                int resultado; // variavel do tipo int para armazenar o resultado
+
+                // verifica se é uma soma
+                if (cmbOperador.Text == "Somar")
+                {
+
+                    resultado = numero1 + numero2;
+                    MessageBox.Show("O resultado da operação: " +"\n" + resultado.ToString(), "Resultado", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+       //corpo do messageBox, \n quebra a linha,  converte o resultado em string para imprimir  Titulo     botoes do MessageBox        icone do MessageBox
+
+                }
+
+                // verifica se é uma subtração
+                if (cmbOperador.Text == "Subtrair")
+                {
+
+                    resultado = numero1 - numero2;
+                    MessageBox.Show(resultado.ToString(), "Resultado", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+
+                }
+
+                // verifica se é divisão
+                if (cmbOperador.Text == "Dividir")
+                {
+
+                    resultado = numero1 / numero2;
+                    MessageBox.Show(resultado.ToString(), "Resultado", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+
+                }
+                // verifica se é multiplicação
+                if (cmbOperador.Text == "Multiplicar")
+                {
+
+                    resultado = numero1 * numero2;
+                    MessageBox.Show(resultado.ToString(), "Resultado", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+
+                }
+
+            }
+
+            // verifica se o operador esta vazio
+            if (cmbOperador.Text == "")
+            {
+                MessageBox.Show("Operado não selecionado", "Erro", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+
+            }
+
+        }
+
+
+    }
+}
